3ea2054e8a6c94b30d3f151783b8a473fed7761f
jurniores <jurniores@gmail.com>
2024-11-05 14:16:33 -0300

    Initial commit

---
+++ Assets/Omni-Networking-for-Unity/Core/Modules/Matchmaking/NetworkGroup.cs
@@ -0,0 +1,395 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using MemoryPack;
+using Newtonsoft.Json;
+using Omni.Shared;
+using Omni.Shared.Collections;
+using static Omni.Core.NetworkManager;
+
+namespace Omni.Core
+{
+    [JsonObject(MemberSerialization.OptIn)]
+    [MemoryPackable]
+    public partial class NetworkGroup : IEquatable<NetworkGroup>
+    {
+        [MemoryPackIgnore]
+        internal readonly Dictionary<int, NetworkPeer> _peersById = new();
+
+        [MemoryPackIgnore]
+        public int Id { get; }
+
+        [MemoryPackIgnore]
+        public string Identifier { get; }
+
+        /// <summary>
+        /// The name of the group(Beautiful identifier).
+        /// </summary>
+        [MemoryPackIgnore]
+        public string Name { get; }
+
+        [MemoryPackIgnore]
+        public int MasterClientId { get; private set; } = NetworkConstants.INVALID_MASTER_CLIENT_ID;
+
+        [MemoryPackIgnore]
+        public int PeerCount => _peersById.Count;
+
+        [MemoryPackIgnore]
+        internal List<NetworkCache> CACHES_APPEND { get; } = new();
+
+        [MemoryPackIgnore]
+        internal Dictionary<int, NetworkCache> CACHES_OVERWRITE { get; } = new();
+
+        [MemoryPackIgnore]
+        public ObservableDictionary<string, object> Data { get; } = new();
+
+        [MemoryPackIgnore, JsonProperty("Data")]
+        public ObservableDictionary<string, object> SerializedData { get; internal set; } = new();
+
+        [MemoryPackIgnore]
+        public Dictionary<int, NetworkPeer> Peers
+        {
+            get
+            {
+                if (__namebuilder__)
+                {
+                    throw new Exception("Peers: Cannot get peers from name builder mode.");
+                }
+
+                return _peersById;
+            }
+        }
+
+        [MemoryPackIgnore]
+        public bool DestroyWhenEmpty { get; set; } = true;
+
+        [MemoryPackIgnore]
+        public bool IsSubGroup => Depth != 1;
+
+        [MemoryPackIgnore]
+        public bool AllowAcrossGroupMessage { get; set; } = true;
+
+        [MemoryPackIgnore]
+        public int Depth { get; }
+
+        [MemoryPackIgnore]
+        private readonly bool __namebuilder__;
+
+        [MemoryPackConstructor]
+        [JsonConstructor]
+        internal NetworkGroup() { }
+
+        public NetworkGroup(string groupName)
+        {
+            string[] subGroups = groupName.Split("->");
+
+            Identifier = groupName;
+            Depth = subGroups.Length;
+            Name = subGroups[Depth - 1];
+            __namebuilder__ = true;
+        }
+
+        internal NetworkGroup(int groupId, string groupName)
+        {
+            string[] subGroups = groupName.Split("->");
+
+            Id = groupId;
+            Identifier = groupName;
+            Depth = subGroups.Length;
+            Name = subGroups[Depth - 1];
+        }
+
+        public NetworkPeer GetPeer(int peerId)
+        {
+            if (__namebuilder__)
+            {
+                throw new Exception("GetPeer: Cannot get peer from name builder mode.");
+            }
+
+            if (_peersById.TryGetValue(peerId, out var peer))
+            {
+                return peer;
+            }
+
+            NetworkLogger.__Log__(
+                $"Group: Peer with ID {peerId}:{Id} not found. Please verify the peer ID and ensure the peer is properly registered(connected!) in the group.",
+                NetworkLogger.LogType.Error
+            );
+
+            return null;
+        }
+
+        public bool TryGetPeer(int peerId, out NetworkPeer peer)
+        {
+            if (__namebuilder__)
+            {
+                throw new Exception("GetPeer: Cannot get peer from name builder mode.");
+            }
+
+            return _peersById.TryGetValue(peerId, out peer);
+        }
+
+        public NetworkGroup AddSubGroup(string subGroupName)
+        {
+            string newIdentifier = $"{Identifier}->{subGroupName}";
+            if (!__namebuilder__)
+            {
+                return Matchmaking.Server.AddGroup(newIdentifier);
+            }
+
+            return new NetworkGroup(newIdentifier);
+        }
+
+        public bool TryAddSubGroup(string subGroupName, out NetworkGroup subGroup)
+        {
+            string newIdentifier = $"{Identifier}->{subGroupName}";
+            if (!__namebuilder__)
+            {
+                return Matchmaking.Server.TryAddGroup(newIdentifier, out subGroup);
+            }
+
+            subGroup = new NetworkGroup(newIdentifier);
+            return true;
+        }
+
+        public void SetMasterClient(NetworkPeer peer)
+        {
+            MasterClientId = peer.Id;
+        }
+
+        public void SyncSerializedData(SyncOptions options)
+        {
+            SyncSerializedData(
+                options.Target,
+                options.DeliveryMode,
+                options.GroupId,
+                options.CacheId,
+                options.CacheMode,
+                options.SequenceChannel
+            );
+        }
+
+        public void SyncSerializedData(
+            Target target = Target.GroupMembers,
+            DeliveryMode deliveryMode = DeliveryMode.ReliableOrdered,
+            int groupId = 0,
+            int cacheId = 0,
+            CacheMode cacheMode = CacheMode.None,
+            byte sequenceChannel = 0
+        )
+        {
+            SyncSerializedData(
+                "_AllKeys_",
+                target,
+                deliveryMode,
+                groupId,
+                cacheId,
+                cacheMode,
+                sequenceChannel
+            );
+        }
+
+        public void SyncSerializedData(string key, SyncOptions options)
+        {
+            SyncSerializedData(
+                key,
+                options.Target,
+                options.DeliveryMode,
+                options.GroupId,
+                options.CacheId,
+                options.CacheMode,
+                options.SequenceChannel
+            );
+        }
+
+        public void SyncSerializedData(
+            string key,
+            Target target = Target.GroupMembers,
+            DeliveryMode deliveryMode = DeliveryMode.ReliableOrdered,
+            int groupId = 0,
+            int cacheId = 0,
+            CacheMode cacheMode = CacheMode.None,
+            byte sequenceChannel = 0
+        )
+        {
+            Internal_SyncSerializedData(
+                key,
+                target,
+                deliveryMode,
+                groupId,
+                cacheId,
+                cacheMode,
+                sequenceChannel
+            );
+        }
+
+        private void Internal_SyncSerializedData(
+            string key = "_AllKeys_",
+            Target target = Target.GroupMembers,
+            DeliveryMode deliveryMode = DeliveryMode.ReliableOrdered,
+            int groupId = 0,
+            int cacheId = 0,
+            CacheMode cacheMode = CacheMode.None,
+            byte sequenceChannel = 0
+        )
+        {
+            if (!IsServerActive)
+            {
+                throw new Exception("Can't use this method on client.");
+            }
+
+            if (MasterClientId <= NetworkConstants.INVALID_MASTER_CLIENT_ID)
+            {
+                throw new Exception(
+                    "MasterClientId is not set. Please set it before using this method."
+                );
+            }
+
+            if (Server.Peers.TryGetValue(MasterClientId, out var peer))
+            {
+                if (SerializedData.TryGetValue(key, out object value) || key == "_AllKeys_")
+                {
+                    value = key != "_AllKeys_" ? value : SerializedData;
+                    ImmutableKeyValuePair keyValuePair = new(key, value);
+                    using var message = Pool.Rent();
+                    message.Write(Id);
+                    message.WriteAsJson(keyValuePair);
+                    Server.SendMessage(
+                        MessageType.SyncGroupSerializedData,
+                        peer,
+                        message,
+                        target,
+                        deliveryMode,
+                        groupId,
+                        cacheId,
+                        cacheMode,
+                        sequenceChannel
+                    );
+                }
+                else
+                {
+                    NetworkLogger.__Log__(
+                        $"SyncSerializedData Group Error: Failed to sync '{key}' because it doesn't exist.",
+                        NetworkLogger.LogType.Error
+                    );
+                }
+            }
+            else
+            {
+                NetworkLogger.__Log__(
+                    $"SyncSerializedData Group Error: Peer(Master) with ID '{MasterClientId}' not found. Please verify the master ID and ensure the peer is properly registered(connected!).",
+                    NetworkLogger.LogType.Error
+                );
+            }
+        }
+
+        public void DeleteCache(CacheMode cacheMode, int cacheId)
+        {
+            if (
+                cacheMode == (CacheMode.Group | CacheMode.New)
+                || cacheMode == (CacheMode.Group | CacheMode.New | CacheMode.AutoDestroy)
+            )
+            {
+                CACHES_APPEND.RemoveAll(x => x.Mode == cacheMode && x.Id == cacheId);
+            }
+            else if (
+                cacheMode == (CacheMode.Group | CacheMode.Overwrite)
+                || cacheMode == (CacheMode.Group | CacheMode.Overwrite | CacheMode.AutoDestroy)
+            )
+            {
+                CACHES_OVERWRITE.Remove(cacheId);
+            }
+            else
+            {
+                NetworkLogger.__Log__(
+                    "Delete Cache Error: Unsupported cache mode set.",
+                    NetworkLogger.LogType.Error
+                );
+            }
+        }
+
+        public void DeleteCache(CacheMode cacheMode, int cacheId, NetworkPeer peer)
+        {
+            if (
+                cacheMode == (CacheMode.Group | CacheMode.New)
+                || cacheMode == (CacheMode.Group | CacheMode.New | CacheMode.AutoDestroy)
+            )
+            {
+                CACHES_APPEND.RemoveAll(x =>
+                    x.Mode == cacheMode && x.Id == cacheId && x.Peer.Id == peer.Id
+                );
+            }
+            else if (
+                cacheMode == (CacheMode.Group | CacheMode.Overwrite)
+                || cacheMode == (CacheMode.Group | CacheMode.Overwrite | CacheMode.AutoDestroy)
+            )
+            {
+                CACHES_OVERWRITE.Remove(cacheId);
+            }
+            else
+            {
+                NetworkLogger.__Log__(
+                    "Delete Cache Error: Unsupported cache mode set.",
+                    NetworkLogger.LogType.Error
+                );
+            }
+        }
+
+        public void DestroyAllCaches(NetworkPeer peer)
+        {
+            CACHES_APPEND.RemoveAll(x => x.Peer.Id == peer.Id && x.AutoDestroyCache);
+            var caches = CACHES_OVERWRITE
+                .Values.Where(x => x.Peer.Id == peer.Id && x.AutoDestroyCache)
+                .ToList();
+
+            foreach (var cache in caches)
+            {
+                if (!CACHES_OVERWRITE.Remove(cache.Id))
+                {
+                    NetworkLogger.__Log__(
+                        $"Destroy All Cache Error: Failed to remove cache {cache.Id} from peer {peer.Id}.",
+                        NetworkLogger.LogType.Error
+                    );
+                }
+            }
+        }
+
+        public void ClearPeers()
+        {
+            _peersById.Clear();
+        }
+
+        public void ClearData()
+        {
+            Data.Clear();
+            SerializedData.Clear();
+        }
+
+        public void ClearCaches()
+        {
+            CACHES_APPEND.Clear();
+            CACHES_OVERWRITE.Clear();
+        }
+
+        public override string ToString()
+        {
+            return ToJson(this);
+        }
+
+        public override bool Equals(object obj)
+        {
+            NetworkGroup other = (NetworkGroup)obj;
+            return Id == other.Id;
+        }
+
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode();
+        }
+
+        public bool Equals(NetworkGroup other)
+        {
+            return Id == other.Id;
+        }
+    }
+}
---
+++ Assets/Omni-Networking-for-Unity/Core/NetworkHelper.cs
@@ -0,0 +1,302 @@
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Omni.Core.Components;
+using Omni.Shared;
+using UnityEngine;
+
+namespace Omni.Core
+{
+    public static class NetworkHelper
+    {
+        private static int d_UniqueId = 1; // 0 - is reserved for server
+
+        // the chances of collision are low, so it's fine to use hashcode.
+        // because... do you have billions of network objects in the scene?
+        // scene objects are different from dynamic objects(instantiated);
+        internal static int GenerateSceneUniqueId()
+        {
+            Guid newGuid = Guid.NewGuid();
+            return newGuid.GetHashCode();
+        }
+
+        // Used for dynamic objects (instantiated).
+        // The chances of collision is zero(0) because each ID is unique(incremental).
+        internal static int GenerateDynamicUniqueId()
+        {
+            if (d_UniqueId == 0)
+            {
+                d_UniqueId = 1;
+            }
+
+            return d_UniqueId++;
+        }
+
+        internal static void Destroy(int identityId, bool isServer)
+        {
+            var identities = isServer
+                ? NetworkManager.Server.Identities
+                : NetworkManager.Client.Identities;
+
+            if (identities.Remove(identityId, out var identity))
+            {
+                if (!isServer && NetworkIdentity.LocalPlayer != null)
+                {
+                    if (NetworkIdentity.LocalPlayer.IdentityId == identityId)
+                    {
+                        NetworkIdentity.LocalPlayer = null;
+                    }
+                }
+
+                NetworkBehaviour[] networkBehaviours =
+                    identity.GetComponentsInChildren<NetworkBehaviour>(true);
+
+                for (int i = 0; i < networkBehaviours.Length; i++)
+                {
+                    networkBehaviours[i].Unregister();
+                }
+
+                UnityEngine.Object.Destroy(identity.gameObject);
+            }
+            else
+            {
+                NetworkLogger.__Log__(
+                    $"Server Destroy: Identity with ID {identity.IdentityId} not found.",
+                    NetworkLogger.LogType.Error
+                );
+            }
+        }
+
+        internal static NetworkIdentity Instantiate(
+            NetworkIdentity prefab,
+            NetworkPeer peer,
+            int identityId,
+            bool isServer,
+            bool isLocalPlayer
+        )
+        {
+            // Disable the prefab to avoid Awake and Start being called multiple times before the registration.
+            prefab.gameObject.SetActive(false);
+
+            NetworkIdentity identity = UnityEngine.Object.Instantiate(prefab);
+            identity.IdentityId = identityId;
+            identity.Owner = peer;
+            identity.IsServer = isServer;
+            identity.IsLocalPlayer = isLocalPlayer;
+            identity._prefabName = prefab.name;
+
+            NetworkBehaviour[] networkBehaviours =
+                identity.GetComponentsInChildren<NetworkBehaviour>(true);
+
+            for (int i = 0; i < networkBehaviours.Length; i++)
+            {
+                NetworkBehaviour networkBehaviour = networkBehaviours[i];
+                networkBehaviour.Identity = identity;
+
+                if (networkBehaviour.Id == 0)
+                {
+                    networkBehaviour.Id = (byte)(i + 1);
+                }
+
+                networkBehaviour.Register();
+                networkBehaviour.OnAwake();
+            }
+
+#if UNITY_EDITOR || !UNITY_SERVER
+            identity.name = $"{prefab.name}(On {(isServer ? "Server" : "Client")})";
+            if (!isServer)
+            {
+                NetworkIsolate[] _ = identity.GetComponentsInChildren<NetworkIsolate>(true);
+                foreach (NetworkIsolate isolate in _)
+                {
+                    UnityEngine.Object.Destroy(isolate);
+                }
+            }
+#endif
+
+            var identities = isServer
+                ? NetworkManager.Server.Identities
+                : NetworkManager.Client.Identities;
+
+            if (!identities.TryAdd(identity.IdentityId, identity))
+            {
+                NetworkLogger.__Log__(
+                    $"Instantiation Error: Failed to add identity with ID '{identity.IdentityId}' to {(isServer ? "server" : "client")} identities. The identity might already exist.",
+                    NetworkLogger.LogType.Error
+                );
+            }
+
+            prefab.gameObject.SetActive(true); // After registration, enable the prefab again.
+            identity.gameObject.SetActive(true); // Enable instantiated object!
+
+            // After Start
+            foreach (var behaviour in networkBehaviours)
+            {
+                behaviour.OnStart();
+            }
+
+            return identity;
+        }
+
+        internal static bool IsPortAvailable(int port, ProtocolType protocolType, bool useIPv6)
+        {
+            try
+            {
+                if (protocolType == ProtocolType.Udp)
+                {
+                    using Socket socket = new Socket(
+                        useIPv6 ? AddressFamily.InterNetworkV6 : AddressFamily.InterNetwork,
+                        SocketType.Dgram,
+                        ProtocolType.Udp
+                    );
+
+                    if (useIPv6)
+                    {
+                        socket.DualMode = true;
+                    }
+
+                    socket.Bind(new IPEndPoint(useIPv6 ? IPAddress.IPv6Any : IPAddress.Any, port));
+                    socket.Close();
+                }
+                else if (protocolType == ProtocolType.Tcp)
+                {
+                    using Socket socket = new Socket(
+                        useIPv6 ? AddressFamily.InterNetworkV6 : AddressFamily.InterNetwork,
+                        SocketType.Stream,
+                        ProtocolType.Tcp
+                    );
+
+                    if (useIPv6)
+                    {
+                        socket.DualMode = true;
+                    }
+
+                    socket.Bind(new IPEndPoint(useIPv6 ? IPAddress.IPv6Any : IPAddress.Any, port));
+                    socket.Close();
+                }
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        internal static int GetAvailablePort(int port, bool useIPv6)
+        {
+            while (!IsPortAvailable(port, ProtocolType.Udp, useIPv6))
+            {
+                port++;
+                if (port > 65535)
+                {
+                    port = 7777;
+                }
+            }
+
+            return port;
+        }
+
+        public static async Task<IPAddress> GetExternalIp(bool useIPv6)
+        {
+            try
+            {
+                using var httpClient = new HttpClient();
+                string externalIp = (
+                    await httpClient.GetStringAsync(
+                        useIPv6 ? "http://ipv6.icanhazip.com/" : "http://ipv4.icanhazip.com/"
+                    )
+                );
+
+                externalIp = externalIp.Replace("\\r\\n", "");
+                externalIp = externalIp.Replace("\\n", "");
+                externalIp = externalIp.Trim();
+
+                if (!IPAddress.TryParse(externalIp, out var ipAddress))
+                {
+                    return IPAddress.Loopback;
+                }
+
+                return ipAddress;
+            }
+            catch
+            {
+                return IPAddress.Loopback;
+            }
+        }
+
+        [Conditional("OMNI_DEBUG")]
+        internal static void EnsureRunningOnMainThread()
+        {
+            if (NetworkManager.MainThreadId != Thread.CurrentThread.ManagedThreadId)
+            {
+                throw new Exception(
+                    "This operation must be performed on the main thread. Omni does not support multithreaded operations. Tip: Dispatch the events to the main thread."
+                );
+            }
+        }
+
+        /// <summary>
+        /// Checks if the given <see cref="GameObject"/> is set to dont destroy on load.
+        /// </summary>
+        /// <param name="gameObject"></param>
+        /// <returns>Returns true if the given <see cref="GameObject"/> is set to dont destroy on load.</returns>
+        public static bool IsDontDestroyOnLoad(GameObject gameObject)
+        {
+            GameObject root = gameObject.transform.root.gameObject;
+            return root.scene.name == "DontDestroyOnLoad"
+                || root.TryGetComponent<NetworkManager>(out _);
+        }
+
+        /// <summary>
+        /// Saves the configuration of a given component to a file in JSON format.
+        /// This method is intended to be used only on the server side.
+        /// </summary>
+        /// <typeparam name="T">The type of the component to be saved.</typeparam>
+        /// <param name="component">The component instance to be saved.</param>
+        /// <param name="fileName">The name of the file where the component's configuration will be saved.</param>
+#if OMNI_DEBUG
+        [Conditional("UNITY_STANDALONE"), Conditional("UNITY_EDITOR")]
+#else
+        [Conditional("UNITY_SERVER"), Conditional("UNITY_EDITOR")]
+#endif
+        public static void SaveComponent<T>(T component, string fileName)
+        {
+            using StreamWriter writer = new(fileName, false);
+            writer.Write(NetworkManager.ToJson(component));
+        }
+
+        /// <summary>
+        /// Loads the configuration from a file and populates the target object with this data.
+        /// This method is intended to be used only on the server side.
+        /// </summary>
+        /// <param name="target">The object to be populated with the configuration data.</param>
+        /// <param name="fileName">The name of the file from which the configuration will be read.</param>
+#if OMNI_DEBUG
+        [Conditional("UNITY_STANDALONE"), Conditional("UNITY_EDITOR")]
+#else
+        [Conditional("UNITY_SERVER"), Conditional("UNITY_EDITOR")]
+#endif
+        public static void LoadComponent(object target, string fileName)
+        {
+            if (File.Exists(fileName))
+            {
+                using StreamReader reader = new(fileName);
+                JsonConvert.PopulateObject(reader.ReadToEnd(), target);
+            }
+        }
+
+        public static void EditorSaveObject(GameObject target)
+        {
+#if UNITY_EDITOR
+            UnityEditor.EditorUtility.SetDirty(target);
+#endif
+        }
+    }
+}
